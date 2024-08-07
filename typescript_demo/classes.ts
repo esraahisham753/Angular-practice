9739077ec3a204ea86e28b73c4b358c2aa948080
Esraa Abduallah <esraahisham753@gmail.com>
2024-08-07 14:43:23 +0000

    Practice inheritance, methods and member visibility

--- typescript_demo/classes.ts
+++ typescript_demo/classes.ts
@@ -1,14 +1,46 @@
 class Employee {
-    id: number;
-    name: string;
+    #id: number;
+    protected name: string;
     address: string;
 
+    get empId() : number {
+        return this.#id;
+    }
+
+    set empId(id: number) {
+        this.#id = id;
+    }
+
+    static getEmployeeCount() : number {
+        return 50;
+    }
+
     constructor(id: number, name: string, address: string) {
-        this.id = id;
+        this.#id = id;
         this.name = name;
         this.address = address;
+    }
+
+    getNameWithAddress() : string {
+        return `${this.name} stays at ${this.address}`;
+    }
+}
+
+class Manager extends Employee {
+    constructor(id: number, name: string, address: string) {
+        super(id, name, address);
+    }
+
+    getNameWithAddress(): string {
+        return `${this.name} is manager at ${this.address}`;
     }
 }
 
 let john = new Employee(1, "John", "Highway 71");
-console.log(john);
+let michael = new Manager(2, "Michael", "New York")
+let address = michael.getNameWithAddress();
+console.log(address);
+Employee.getEmployeeCount();
+michael.empId = 100;
+console.log(michael.empId);
+
