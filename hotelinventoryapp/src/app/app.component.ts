521e8e49773e39edcf28a016d9753405c448de32
Esraa Abduallah <esraahisham753@gmail.com>
2024-08-09 14:55:57 +0000

    change main template

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -6,7 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  //template: `
+  //<h1>Hello, World from inline template</h1>
+  //<p>Awesome angular app</p>
+  //`,
   styleUrl: './app.component.scss'
+  //styles: [`h1 {color: red}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
