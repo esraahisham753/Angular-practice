b6271b194cfcbf6e836c532cb74fae2ba6d3fc4e
Esraa Abduallah <esraahisham753@gmail.com>
2024-08-12 12:40:33 +0000

    Add roomlist interface, data and table heading

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Rooms } from './rooms';
+import { RoomList, Rooms } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -18,6 +18,35 @@ export class RoomsComponent {
     availableRooms: 10,
     bookedRooms: 5
   };
+
+  roomList : RoomList[] = [{
+    roomNumber: 1,
+    roomType: "Deluxe Room",
+    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+    price: 500,
+    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+    checkIn: new Date('12-Aug-2024'),
+    checkOut: new Date('13-Aug-2024')
+  },
+  {
+    roomNumber: 2,
+    roomType: "Deluxe Room",
+    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+    price: 1000,
+    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+    checkIn: new Date('14-Aug-2024'),
+    checkOut: new Date('15-Aug-2024')
+  },
+  {
+    roomNumber: 3,
+    roomType: "Private suite",
+    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+    price: 1500,
+    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+    checkIn: new Date('16-Aug-2024'),
+    checkOut: new Date('17-Aug-2024')
+  }
+];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
--- hotelinventoryapp/src/app/rooms/rooms.ts
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -3,3 +3,13 @@ export interface Rooms {
   availableRooms: number;
   bookedRooms: number;
 }
+
+export interface RoomList {
+  roomNumber: number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photo: string;
+  checkIn: Date;
+  checkOut: Date;
+}
