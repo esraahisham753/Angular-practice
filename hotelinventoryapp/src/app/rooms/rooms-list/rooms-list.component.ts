0c09527d838a040e440441edbf115b750c55f77b
Esraa Abduallah <esraahisham753@gmail.com>
2024-08-14 16:14:16 +0000

    implement Rooms OnInit and create RoomsList component

---
+++ hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomList } from '../rooms';
+
+@Component({
+  selector: 'hinv-rooms-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rooms-list.component.html',
+  styleUrl: './rooms-list.component.scss'
+})
+export class RoomsListComponent {
+  @Input() rooms : RoomList[] = [];
+}
--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomList, Rooms } from './rooms';
 
@@ -9,7 +9,7 @@ import { RoomList, Rooms } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss',
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
   hotelname = 'Hilton Hotel';
   numberOfRooms = 10;
   hideRooms = false;
@@ -19,37 +19,41 @@ export class RoomsComponent {
     bookedRooms: 5
   };
 
-  roomList : RoomList[] = [{
-    roomNumber: 1,
-    roomType: "Deluxe Room",
-    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
-    price: 500,
-    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
-    checkIn: new Date('12-Aug-2024'),
-    checkOut: new Date('13-Aug-2024'),
-    rating: 4.5
-  },
-  {
-    roomNumber: 2,
-    roomType: "Deluxe Room",
-    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
-    price: 1000,
-    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
-    checkIn: new Date('14-Aug-2024'),
-    checkOut: new Date('15-Aug-2024'),
-    rating: 3.46546
-  },
-  {
-    roomNumber: 3,
-    roomType: "Private suite",
-    amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
-    price: 1500,
-    photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
-    checkIn: new Date('16-Aug-2024'),
-    checkOut: new Date('17-Aug-2024'),
-    rating: 2.6
+  roomList : RoomList[] = [];
+
+  ngOnInit(): void {
+    this.roomList = [{
+      roomNumber: 1,
+      roomType: "Deluxe Room",
+      amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+      price: 500,
+      photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+      checkIn: new Date('12-Aug-2024'),
+      checkOut: new Date('13-Aug-2024'),
+      rating: 4.5
+    },
+    {
+      roomNumber: 2,
+      roomType: "Deluxe Room",
+      amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+      price: 1000,
+      photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+      checkIn: new Date('14-Aug-2024'),
+      checkOut: new Date('15-Aug-2024'),
+      rating: 3.46546
+    },
+    {
+      roomNumber: 3,
+      roomType: "Private suite",
+      amenities: "Air conditioner, Wi-Fi, bathroom, kitchen",
+      price: 1500,
+      photo: 'https://unsplash.com/photos/vacant-white-bed-near-the-window-B4rEJ09-Puo',
+      checkIn: new Date('16-Aug-2024'),
+      checkOut: new Date('17-Aug-2024'),
+      rating: 2.6
+    }
+  ];
   }
-];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
